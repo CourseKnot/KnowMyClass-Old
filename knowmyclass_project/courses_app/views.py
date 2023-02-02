4017cb0da277ff9c63a7b490562ae34741dad665
Yubo Jing <jybjybjing@YubodeMacBook-Air.local>
2023-02-02 13:33:30 -0500

    add serializers and modify views.py

--- knowmyclass_project/courses_app/views.py
+++ knowmyclass_project/courses_app/views.py
@@ -1,14 +1,14 @@
 from django.shortcuts import render
 from .models import Course, Department, University
+from rest_framework import viewsets   
+from .serializers import CourseSerializer                 
 
 
-from rest_framework import viewsets
-from .serializers import CourseSerializer
-from .models import Course
 
-class CourseViewSet(viewsets.ModelViewSet):
-    queryset = Course.objects.all()
+class CourseView(viewsets.ModelViewSet):
     serializer_class = CourseSerializer
+    queryset = Course.objects.all()
+
 
 
 def course_list(request):
