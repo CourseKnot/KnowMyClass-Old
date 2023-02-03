1f29b3c4a0137454d696ed07dd14de2f6529894b
Yubo Jing <jybjybjing@YubodeMacBook-Air.local>
2023-02-03 11:20:40 -0500

    connect react-django app

--- knowmyclass_project/courses_app/urls.py
+++ knowmyclass_project/courses_app/urls.py
@@ -1,9 +1,9 @@
-from django.urls import path
-from .views import course_list, department_list, university_list
+# from django.urls import path
+# from .views import course_list, department_list, university_list, CourseView
 
-app_name = 'courses_app'
-urlpatterns = [
-    path('courses/', course_list, name='course_list'),
-    path('departments/', department_list, name='department_list'),
-    path('universities/', university_list, name='university_list'),
-]
+# app_name = 'courses_app'
+# urlpatterns = [
+#     path('courses/', CourseView, name='course_list'),
+#     path('departments/', department_list, name='department_list'),
+#     path('universities/', university_list, name='university_list'),
+# ]
