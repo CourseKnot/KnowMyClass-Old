09e208009cc6cd064dd43b4dac1c4f7587df449e
posecond <cellsinterlinked@icloud.com>
2023-02-03 22:03:32 -0500

    department_list

--- bulletin_crawler/crawler.py
+++ bulletin_crawler/crawler.py
@@ -5,12 +5,22 @@ import re
 import requests
 import json
 
-# bulletin base url
-base_url = 'https://bulletins.psu.edu'
-base_html = requests.get(base_url+'/university-course-descriptions/undergraduate/')
+# undergraduate bulletin base url
+base_url = 'https://bulletins.psu.edu/university-course-descriptions/undergraduate/'
+base_html = requests.get(base_url)
 
 # get subpage url
-subpage_pattern = re.compile('/university-course-descriptions/undergraduate/[a-z]+/{1}')
-sub_url_list = subpage_pattern.findall(base_html.text)
-f = open('text.txt','w')
-f.write(str(sub_url_list))
+department_pattern = re.compile('/university-course-descriptions/undergraduate/([a-z]+)/{1}')
+department_list = department_pattern.findall(base_html.text)
+
+# write department list (abbrev)
+f = open('./bulletin_crawler/department_list.txt','w')
+for dep in department_list:
+    f.write(dep+'\n')
+f.close()
+
+# access each departments
+# for i in range(2):
+#     info_dict = {}
+#     sub_html = requests.get(base_url+sub_url_list[i])
+#     title_pattern = re.compile('<div class="course_codetitle">.+')
