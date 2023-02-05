5bde9c21135db7b6a583cd9a6af8ae9c4aa5f511
Yubo Jing <jybjybjing@YubodeMacBook-Air.local>
2023-02-05 18:08:04 -0500

    admin register change

--- backend/knowmyclass/admin.py
+++ backend/knowmyclass/admin.py
@@ -4,4 +4,13 @@ from .models import Course, Department
 # Register your models here.
 
 admin.site.register(Course)
-admin.site.register(Department)
+
+class CourseInline(admin.TabularInline):
+    model = Course
+    fields = ('course_name',)
+    extra = 1
+
+class DepartmentAdmin(admin.ModelAdmin):
+    inlines = [CourseInline]
+
+admin.site.register(Department, DepartmentAdmin)
