80341a4b8375d2ed12adaa5a13caa3266abe0e4a
posecond <cellsinterlinked@icloud.com>
2023-02-24 01:03:44 -0500

    extract_courseforum update

--- scripts/gpt3_test/extract_courseforum.py
+++ scripts/gpt3_test/extract_courseforum.py
@@ -4,13 +4,19 @@ import json
 from bs4 import BeautifulSoup
 import os
 
-if __name__ == '__main__':
-    base_html = requests.get('https://thecourseforum.com/course/603/7735/')
+output_dir = 'output.txt'
+url = 'https://thecourseforum.com/course/603/7735/'
+
+def extract_comments():
+    base_html = requests.get(url)
     soup = BeautifulSoup(base_html.content,"html.parser")
     comment_block = soup.find_all('div', class_='review-text-full')
-    comment_list  = []
-    for comment in comment_block:
-        comment_list.append(comment.text)
-    with open('output.txt', 'w') as file:
-        file.write(str(comment_list))
+    res = [comment.text for comment in comment_block]
+    return res
+
+if __name__ == '__main__':
+    comment_list = extract_comments()
+    
     print("Comments count: {}".format(len(comment_list)))
+    with open(output_dir, 'w') as file:
+        file.write(str(comment_list)) 
