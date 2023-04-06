9cd914bc2397cc02290c63ba1098f9628caeae97
verityyywang <yjw5225@psu.edu>
2023-04-06 17:28:00 -0400

    modify font size

--- frontend/src/components/OverallRating.js
+++ frontend/src/components/OverallRating.js
@@ -5,14 +5,16 @@ import overallRatingIcon from '../assets/overall-rating-icon.png';
 const OverallRating = (props) => {
     return (
         <div className='overall-rating'>
+            <div className='overall-rating-rate'>
+                <div className='overall-rating-actual-rate'>
+                    <p>{props.rate}</p>
+                </div>
+                <div className='overall-rating-full-rate'>
+                    <p>/5</p>
+                </div>
+            </div>
             <div className='overall-rating-text'>
                 <p>Overall Rating</p>
-            </div>
-            <div className='overall-rating-actual-rate'>
-                <p>{props.rate}</p>
-            </div>
-            <div className='overall-rating-full-rate'>
-                <p>/5</p>
             </div>
             <div className='overall-rating-icon'>
                 <img src={overallRatingIcon} alt="overall-rating-icon" />
