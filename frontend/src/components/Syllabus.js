a3f1ac098e1248c80dd9b4acbc4c9a379bdf346d
Yubo Jing <jybjybjing@YubodeMacBook-Air.local>
2023-04-07 16:54:29 -0400

    update syllabus

--- frontend/src/components/Syllabus.js
+++ frontend/src/components/Syllabus.js
@@ -1,30 +1,13 @@
-import React from 'react';
-import './css/Syllabus.css'
+import React, { useState } from 'react';
+import './css/Syllabus.css';
 import { Icon } from '@iconify/react';
 import Card from 'react-bootstrap/Card';
-
-// const Syllabus = (props) => {
-//     return (
-//         <div className='syllabus'>
-//             <div className='syllabus-text'>
-//                 <p>{props.text}</p>
-//             </div>
-            // <div className='syllabus-icon'>
-            //     <Icon icon="mdi:file-document-outline" color="#1b1c1e" width="69" height="69" />
-            // </div>
-            // <div className='syllabus-date'>
-            //     <p>{props.date}</p>
-            // </div>
-//         </div>
-//     );
-// };
-
-// export default Syllabus;
-
+import Modal from 'react-bootstrap/Modal';
+import Button from 'react-bootstrap/Button';
 
 const cardStyle = {
-    width: '210px', /* Set the width of the card */
-    height: '210px', /* Set the height of the card */
+    width: '210px',
+    height: '210px',
     background: "#FFFFFF",
     boxShadow: "0px 4px 20px rgba(0, 0, 0, 0.15)",
     borderRadius: "10px",
@@ -32,27 +15,54 @@ const cardStyle = {
 };
 
 function Syllabus(props) {
+    const [showModal, setShowModal] = useState(false);
+
+    const handleClick = () => {
+        setShowModal(true);
+    };
+
+    const handleClose = () => {
+        setShowModal(false);
+    };
+
     return (
-      <div className='syllabus-wrapper'>
-        <Card className='border-0' style={cardStyle}>
-          <Card.Body>
-            <Card.Text>
-                <div className='syllabus'>
-                    <div className='syllabus-text'>
-                        <p>{props.text}</p>
-                    </div>
-                    <div className='syllabus-icon'>
-                        <Icon icon="mdi:file-document-outline" color="#1b1c1e" width="69" height="69" />
-                    </div>
-                    <div className='syllabus-date'>
-                        <p>{props.date}</p>
-                    </div>
-                </div>
-            </Card.Text>
-          </Card.Body>
-        </Card>
-      </div>
+        <div className='syllabus-wrapper'>
+            <Card className='border-0 card-clickable' style={cardStyle} onClick={handleClick}>
+                <Card.Body>
+                    <Card.Text>
+                        <div className='syllabus'>
+                            <div className='syllabus-text'>
+                                <p>{props.text}</p>
+                            </div>
+                            <div className='syllabus-icon'>
+                                <Icon icon="mdi:file-document-outline" color="#1b1c1e" width="69" height="69" />
+                            </div>
+                            <div className='syllabus-date'>
+                                <p>{props.date}</p>
+                            </div>
+                        </div>
+                    </Card.Text>
+                </Card.Body>
+            </Card>
+
+            <Modal show={showModal} onHide={handleClose}
+                  size="lg"
+                  aria-labelledby="contained-modal-title-vcenter"
+                  centered>
+                <Modal.Header closeButton>
+                    <Modal.Title>CMPSC 465 Data Structures and Algorithms Syllabus</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                    <p>Modal body text goes here.</p>
+                </Modal.Body>
+                <Modal.Footer>
+                    <Button variant="secondary" onClick={handleClose}>
+                        Close
+                    </Button>
+                </Modal.Footer>
+            </Modal>
+        </div>
     );
-  }
+}
 
 export default Syllabus;
